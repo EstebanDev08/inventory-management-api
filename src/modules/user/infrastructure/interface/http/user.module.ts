618eb4bb45f0c752e286dd86696c2e8683bce6b7
Esteban Solarte <estebansolarte86@gmail.com>
2025-06-21 12:02:51 -0500

    feat(user): add ValidateUserPasswordUseCase and update UserModule exports

--- src/modules/user/infrastructure/interface/http/user.module.ts
+++ src/modules/user/infrastructure/interface/http/user.module.ts
@@ -3,6 +3,8 @@ import { Module } from '@nestjs/common';
 import { CreateUserService } from '#src/modules/user/app/service/create-user.service';
 import { CreateCustomerUseCase } from '#src/modules/user/app/usecases/create-customer/create-customer.usecase';
 import { CreateSellerUseCase } from '#src/modules/user/app/usecases/create-seller/create-seller.usecase';
+import { GetUserByEmailuseCase } from '#src/modules/user/app/usecases/get-by-email/get-by-email.usecase';
+import { ValidateUserPasswordUseCase } from '#src/modules/user/app/usecases/validate-password/validate-password.usecase';
 import { IUserRepository } from '#src/modules/user/domain/user.repository';
 import { DrizzleTransactionService } from '#src/shared/database/drizzle/service/drizzleTransaction.service';
 import { ITransactionService } from '#src/shared/database/transaction.interface';
@@ -24,8 +26,10 @@ import { CreateSellerController } from './controller/create-seller.controller';
     CreateUserService,
     CreateCustomerUseCase,
     CreateSellerUseCase,
+    GetUserByEmailuseCase,
+    ValidateUserPasswordUseCase,
   ],
   controllers: [CreateCustomerController, CreateSellerController],
-  exports: [InMemoryRepository, EncryptImpl],
+  exports: [InMemoryRepository, GetUserByEmailuseCase, ValidateUserPasswordUseCase],
 })
 export class UserModule {}
